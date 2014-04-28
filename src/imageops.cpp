8b437242203c0dcdef49b8080c2a7abda7fd49f1
Andrej Lajovic <andrej.lajovic@ad-vega.si>
2014-04-28 15:02:20 +0200

    Restrict individual components of subpixel correction rather than the norm

--- src/imageops.cpp
+++ src/imageops.cpp
@@ -306,13 +306,13 @@ Mat1f findShifts(const Mat& img,
       subPixelMin = coarseMin;
       quadraticFit qf(match, coarseMin);
       Point2f subShift = qf.minimum();
-      if (norm(subShift) > 0.5) {
+      if (subShift.x > 0.5 || subShift.y > 0.5) {
         // Subpixel correction larger than 0.5 px indicates poor fit. Project
         // out the direction corresponding to the smaller eigenvalue and see
         // if that helps.
         subShift = subShift.dot(qf.largerEigVec()) * qf.largerEigVec();
         // Give up if the shift is still larger than 0.5 px.
-        if (norm(subShift) > 0.5)
+        if ((subShift.x > 0.5 || subShift.y > 0.5))
           subShift = Point2f(0, 0);
       }
       subPixelMin += subShift;
